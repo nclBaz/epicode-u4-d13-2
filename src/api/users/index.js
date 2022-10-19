88ae4d30ec4e83fe35f7c6572df2e188559783ec
Riccardo Gulin <riccardo@nucleode.com>
2022-10-19 10:07:51 +0200

    Adds yesterday's stuff

---
+++ src/api/users/index.js
@@ -0,0 +1,79 @@
+import e from "express"
+import express from "express"
+import createHttpError from "http-errors"
+import UsersModel from "./model.js"
+
+const usersRouter = express.Router()
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new UsersModel(req.body) // here it happens validation (thanks to Mongoose) of req.body, if it is not ok Mongoose will throw an error
+    const { _id } = await newUser.save()
+    res.status(201).send({ _id })
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await UsersModel.find({}, { firstName: 1 })
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.params.userId)
+    if (user) {
+      res.send(user)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put("/:userId", async (req, res, next) => {
+  try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.params.userId, // WHO you want to modify
+      req.body, // HOW you want to modify
+      { new: true, runValidators: true } // OPTIONS. By default findByIdAndUpdate returns the record PRE-MODIFICATION. If you want to get back the updated object --> new:true
+      // By default validation is off here --> runValidators: true
+    )
+
+    // ******************************************************** ALTERNATIVE METHOD **************************************************
+    /*     const user = await UsersModel.findById(req.params.userId) // When you do a findById, findOne, etc,... you get back not a PLAIN JS OBJECT but you obtain a MONGOOSE DOCUMENT which is an object with some superpowers
+
+    user.firstName = "George"
+
+    await user.save()
+    res.send(user) */
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/:userId", async (req, res, next) => {
+  try {
+    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId)
+    if (deletedUser) {
+      res.status(204).send()
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default usersRouter
---
+++ src/api/users/model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+const usersSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    age: { type: Number, required: true, min: 18, max: 65 },
+    professions: [String],
+    address: {
+      street: { type: String },
+      number: { type: Number },
+    },
+  },
+  {
+    timestamps: true, // this option automatically handles createdAt and updatedAt fields
+  }
+)
+
+export default model("User", usersSchema) // this model is now binded to the "users" collection, if the collection does not exist, mongoose will create it automagically
